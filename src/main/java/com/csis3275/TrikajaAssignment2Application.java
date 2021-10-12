b36d54662e52cfa9fceb67230d57a138f33090b8
Tristan Vizvary <vizvaryt@student.douglascollege.ca>
2021-10-12 05:51:38 -0700

    Intial files with pom.xml and application properties updated

---
+++ src/main/java/com/csis3275/TrikajaAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrikajaAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrikajaAssignment2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/csis3275/TrikajaAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrikajaAssignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
